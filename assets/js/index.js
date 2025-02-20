1a286f1106509dcb43bc81405723f21529fd9893
ROW <axlrower@gmail.com>
2025-02-19 22:50:13 -0300

    resolucion del primer problema

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+let luna = document.getElementById("imagen");
+
+function borde() {
+  if (luna.style.border) {
+    luna.style.border = "";
+  } else {
+    luna.style.border = "30px solid yellow";
+  }
+
+  
+}
