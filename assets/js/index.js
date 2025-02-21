15a466dff059c961f52f0b9c982d18df7d6e4968
ROW <axlrower@gmail.com>
2025-02-20 22:32:52 -0300

    iniciación del tercer ejercicio

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,33 +7,24 @@ function borde() {
   } else {
     luna.style.border = "30px solid yellow";
   }
-
-  
 }
 
 // EJERCICIO NRO 2
 
-function calculo(){
-let cascoA = Number(document.getElementById("helmet1").value);
-let cascoB = Number(document.getElementById("helmet2").value);
-let cascoC = Number(document.getElementById("helmet3").value);
-let cascoD = Number(document.getElementById("helmet4").value);
-let cascoE = Number(document.getElementById("helmet5").value);
-let cascoF = Number(document.getElementById("helmet6").value);
+function calculo() {
+  let cascoA = Number(document.getElementById("helmet1").value);
+  let cascoB = Number(document.getElementById("helmet2").value);
+  let cascoC = Number(document.getElementById("helmet3").value);
+  let cascoD = Number(document.getElementById("helmet4").value);
+  let cascoE = Number(document.getElementById("helmet5").value);
+  let cascoF = Number(document.getElementById("helmet6").value);
 
-let total = cascoA + cascoB + cascoC + cascoD + cascoE + cascoF;
-let usuarioMensaje = document.getElementById("mensajeUsuario");
+  let total = cascoA + cascoB + cascoC + cascoD + cascoE + cascoF;
+  let usuarioMensaje = document.getElementById("mensajeUsuario");
 
-
-
-    if(total <= 10){
-        usuarioMensaje.innerText =" Llevas " + total + " cascos";
-
-    }else {
-        usuarioMensaje.innerText = "Llevas demasiados cascos";
-
-
-    }
-
+  if (total <= 10) {
+    usuarioMensaje.innerText = " Llevas " + total + " cascos";
+  } else {
+    usuarioMensaje.innerText = "Llevas demasiados cascos";
+  }
 }
-
