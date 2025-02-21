f3268cc317be997a5a49bbab38355db5723e885b
ROW <axlrower@gmail.com>
2025-02-20 22:18:52 -0300

    resolucion del 2do ejercicio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// EJERCICIO NRO 1
 let luna = document.getElementById("imagen");
 
 function borde() {
@@ -9,3 +10,30 @@ function borde() {
 
   
 }
+
+// EJERCICIO NRO 2
+
+function calculo(){
+let cascoA = Number(document.getElementById("helmet1").value);
+let cascoB = Number(document.getElementById("helmet2").value);
+let cascoC = Number(document.getElementById("helmet3").value);
+let cascoD = Number(document.getElementById("helmet4").value);
+let cascoE = Number(document.getElementById("helmet5").value);
+let cascoF = Number(document.getElementById("helmet6").value);
+
+let total = cascoA + cascoB + cascoC + cascoD + cascoE + cascoF;
+let usuarioMensaje = document.getElementById("mensajeUsuario");
+
+
+
+    if(total <= 10){
+        usuarioMensaje.innerText =" Llevas " + total + " cascos";
+
+    }else {
+        usuarioMensaje.innerText = "Llevas demasiados cascos";
+
+
+    }
+
+}
+
